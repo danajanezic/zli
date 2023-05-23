a8e3b56bfe02a85f202e124f603a8bad8164d980
Dana Janezic <danajanezic@gmail.com>
2023-05-22 17:12:18 -0700

    More minor updates

--- types/index.js
+++ types/index.js
@@ -2,7 +2,6 @@ const base = {
   name: 'string',
   message: 'string',
   description: 'string',
-  callback: 'function',
   required: false,
 };
 
@@ -17,30 +16,22 @@ export const OPTION_TYPES = {
   BOOLEAN: {
     ...base,
     inputType: INPUT_TYPES.boolean,
-    promptType: 'confirm',
   },
   INPUT: {
     ...base,
     inputType: INPUT_TYPES.scalar,
-    promptType: 'input',
   },
   PASSWORD: {
     ...base,
     inputType: INPUT_TYPES.password,
-    promptType: 'password',
   },
   ONE_OF: {
     ...base,
     inputType: 'scalar',
-    promptType: 'autocomplete',
-    multiple: false,
   },
   ANY_OF: {
     ...base,
     inputType: INPUT_TYPES.variadicString,
-    choices: 'variadic',
-    promptType: 'autocomplete',
-    multiple: true,
   },
   VARIADIC: {
     ...base,
